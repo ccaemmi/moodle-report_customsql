664d076b0bab5b962c8d76ec1452efdf71458889
ccaemmi <ccaemmi@ucl.ac.uk>
2023-06-15 15:33:38 +0100

    adding require once get_users.php

--- tests/external/external_get_users_test.php
+++ tests/external/external_get_users_test.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+require_once(__DIR__ . '/../../external/get_users.php');
 namespace report_customsql\external;
 
 defined('MOODLE_INTERNAL') || die();
