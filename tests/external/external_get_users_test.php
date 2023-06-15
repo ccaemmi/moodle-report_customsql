3fd8c1dbfc071fc00dedbea061cffc21d881141e
ccaemmi <ccaemmi@ucl.ac.uk>
2023-06-15 15:28:47 +0100

    workflows

--- tests/external/external_get_users_test.php
+++ tests/external/external_get_users_test.php
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-
 namespace report_customsql\external;
 
 defined('MOODLE_INTERNAL') || die();
