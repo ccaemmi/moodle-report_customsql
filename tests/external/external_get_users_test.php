10b9e7370d7ec2c7bed4f99c263096be6d7e3b12
ccaemmi <ccaemmi@ucl.ac.uk>
2023-06-15 16:43:01 +0100

    run in separate process

--- tests/external/external_get_users_test.php
+++ tests/external/external_get_users_test.php
@@ -31,6 +31,7 @@ require_once($CFG->dirroot . '/webservice/tests/helpers.php');
  * @category  external
  * @copyright 2020 The Open University
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @runTestsInSeparateProcesses
  */
 class external_get_users_test extends \externallib_advanced_testcase {
 
@@ -66,7 +67,6 @@ class external_get_users_test extends \externallib_advanced_testcase {
         return [$admin, $manager, $coursecreateor];
     }
 
-    // @runInSeparateProcesses
     public function test_get_users_site_config() {
         [$admin] = $this->setup_users();
         $defaultuserimage = 'https://www.example.com/moodle/theme/image.php/_s/boost/core/1/u/f2';
